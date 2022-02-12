34c7c925a210fc15d3981717c38a9882aa72235b
Nikolay <tehlic@gmail.com>
2022-02-12 15:39:29 +0300

    First attempt to catch node and Meteor errors at once

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log('error catcher')
+
+const enableErrorCatcher = ({
+  errorCatcher
+}) => {
+  if (Meteor.isServer) {
+    const bound = Meteor.bindEnvironment((callback) => {
+      callback();
+    });
+
+    process.on('uncaughtException', function (err) {
+      bound(() => {
+        errorCatcher('process uncaughtException', err);
+        process.exit(7);
+      });
+    });
+
+    // Catch all meteor's errors by hijacking Meteor._debug
+    // Save a link
+    const originalMeteorDebug = Meteor._debug;
+    
+    Meteor._debug = function (message, stack) {
+
+      console.log('error happened');
+
+      errorCatcher(message, stack);
+
+      // Chain
+      return originalMeteorDebug.apply(this, arguments); 
+    };
+  }
+};
+
+
+
+export {
+  enableErrorCatcher
+}
