0bd4c0f59c5db882cf643af64354e00d5339ac31
Nikolay <tehlic@gmail.com>
2024-09-03 09:57:49 +0400

    feat: Add console.logs and ts

--- index.ts
+++ index.ts
@@ -1,27 +1,59 @@
+// @ts-ignore
 import { Meteor } from 'meteor/meteor';
 
-const enableErrorCatcher = ({ errorCatcher }) => {
+interface ErrorCatcherErrorHandleParams {
+  errorSource: 'meteor.debug' | 'node';
+  error: object;
+}
+
+interface ErrorCatcherParams {
+  handleError: (params: ErrorCatcherErrorHandleParams) => void;
+}
+
+/**
+ * Enables universal error catcher for Node and Meteor
+ * uncaught exceptions
+ */
+const enableErrorCatcher = async ({ handleError }: ErrorCatcherParams) => {
   if (Meteor.isServer) {
-    const bound = Meteor.bindEnvironment((callback) => {
-      callback();
-    });
+    console.log('Error Catcher Enabled');
 
-    process.on('uncaughtException', function (err) {
-      bound(() => {
-        errorCatcher('process uncaughtException', err);
-        process.exit(7);
+    /**
+     * Exit code 7
+     * 7 - Internal Exception Handler Run-Time Failure:
+     * There was an uncaught exception, and the internal fatal exception handler function itself
+     * threw an error while attempting to handle it.
+     */
+
+    process.on('uncaughtException', async (error) => {
+      console.error(error);
+
+      await handleError({
+        errorSource: 'node',
+        error,
       });
+
+      /** Make sure this is called last */
+      setTimeout(() => process.exit(7), 0);
     });
 
-    // Catch all meteor's errors by hijacking Meteor._debug
-    // Save a link
+    /** Catch all meteor's errors by hijacking Meteor._debug */
     const originalMeteorDebug = Meteor._debug;
 
-    Meteor._debug = function (message, stack) {
-      errorCatcher(message, stack);
+    Meteor._debug = async function (message, stack) {
+      console.error(message, stack);
 
-      // Chain
-      return originalMeteorDebug.apply(this, arguments);
+      const error = new Error(message, stack || []);
+
+      await handleError({
+        errorSource: 'meteor.debug',
+        error,
+      });
+
+      return originalMeteorDebug.apply(
+        this,
+        Array.prototype.slice.call(arguments)
+      );
     };
   }
 };
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'kolyasya:meteor-error-catcher',
-  version: '0.0.1-beta.1',
+  version: '0.0.1-beta.3',
   summary: 'Catch all server errors',
   git: 'https://github.com/kolyasya/meteor-error-catcher.git',
   documentation: './README.md',
@@ -9,6 +9,7 @@ Package.describe({
 Package.onUse((api) => {
   api.use('ecmascript@0.16.8');
   api.use('typescript@5.4.3');
+  api.use('zodern:types@1.0.13');
 
   api.mainModule('./index.ts', ['server']);
 });
